452933ba9caa35c85e3b83f9b9a30067c4dc9990
Felix Kitaka <webash.uganda@gmail.com>
2013-11-22 09:01:45 +0000

    [Felix,MJ] Updated geoserver poll data to support translated yes / no polls

--- geoserver/management/commands/export_poll_data.py
+++ geoserver/management/commands/export_poll_data.py
@@ -6,24 +6,35 @@ from geoserver.models import PollData, PollCategoryData, PollResponseData
 from poll.models import Poll
 from rapidsms.contrib.locations.models import Location
 from django.conf import settings
+from django.utils import translation
+from django.utils.translation import ugettext
+import operator
 
 class Command(BaseCommand):
 
     help = """loads yes no polls into the poll data for geoserver"""
 
     def handle(self, **options):
+        translation.activate(settings.LANGUAGE_CODE)
+
         root = Location.tree.root_nodes()[0]
         yesno_category_name = ['yes', 'no', 'unknown']
+        yesno_category_name_translated = {ugettext('yes'):"yes", ugettext('no'):'no', ugettext('unknown'):'unknown'}
         for p in Poll.objects.order_by('-pk')[0:9]:
             if p.categories.count():
-                category_names = yesno_category_name if p.is_yesno_poll() else list(p.categories.all().values_list('name', flat=True))
+                is_yesno_poll = p.is_yesno_poll()
+                category_names = yesno_category_name if is_yesno_poll else list(p.categories.all().values_list('name', flat=True))
                 category_names.append('uncategorized')
                 data = p.responses_by_category(location=root)
+
                 insert_data = {}
                 for d in data:
                     l = Location.objects.get(pk=d['location_id'])
                     insert_data.setdefault(l.code, {})
-                    insert_data[l.code][d['category__name']] = d['value']
+                    if is_yesno_poll:
+                        insert_data[l.code][yesno_category_name_translated[d['category__name']]] = d['value']
+                    else:
+                        insert_data[l.code][d['category__name']] = d['value']
                 for district_code, values in insert_data.items():
                     total = 0
                     for c in category_names:
@@ -35,19 +46,15 @@ class Command(BaseCommand):
                     top_category = 0
                     max_category = 0
                     cat_num = 0
-                    # import pdb;pdb.set_trace()
-                    print values
                     for cat_name in category_names:
                         values[cat_name] = float(values[cat_name]) / total
                         if values[cat_name] > max_category:
                             top_category = cat_num
                             max_category = values[cat_name]
                         cat_num += 1
-                    import operator
+
                     top_category=category_names.index(max(values.iteritems(), key=operator.itemgetter(1))[0])
-                    print max(values.iteritems(), key=operator.itemgetter(1))[0]
-                    print top_category
-                    if p.is_yesno_poll():
+                    if is_yesno_poll:
                         pd, _ = PollData.objects.using('geoserver').get_or_create(\
                             district=district_code,\
                             poll_id=p.pk,\
